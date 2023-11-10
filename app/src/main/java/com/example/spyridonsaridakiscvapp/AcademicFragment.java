f7b90bcf45112b9271dbabdc9adf20478d87104f
SpirosSaridakis <spiros.sar1234@hotmail.com>
2023-11-10 17:44:04 +0200

    Created the menu, added a picture and changed the navigation menu stuff

--- app/src/main/java/com/example/spyridonsaridakiscvapp/AcademicFragment.java
+++ app/src/main/java/com/example/spyridonsaridakiscvapp/AcademicFragment.java
@@ -1,4 +1,4 @@
-package com.example.spyridonsaridakiscvapp.ui.gallery;
+package com.example.spyridonsaridakiscvapp;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,24 +8,20 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.spyridonsaridakiscvapp.databinding.FragmentGalleryBinding;
 
-public class GalleryFragment extends Fragment {
+public class AcademicFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
 
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/spyridonsaridakiscvapp/VolunteeringFragment.java
+++ app/src/main/java/com/example/spyridonsaridakiscvapp/VolunteeringFragment.java
@@ -1,4 +1,4 @@
-package com.example.spyridonsaridakiscvapp.ui.slideshow;
+package com.example.spyridonsaridakiscvapp;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,24 +8,19 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.spyridonsaridakiscvapp.databinding.FragmentSlideshowBinding;
 
-public class SlideshowFragment extends Fragment {
+public class VolunteeringFragment extends Fragment {
 
     private FragmentSlideshowBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        SlideshowViewModel slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
-
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
