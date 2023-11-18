086d9c3265f747f5e75dc292ff4d57d05c215be8
SpirosSaridakis <spiros.sar1234@hotmail.com>
2023-11-18 12:37:22 +0200

    Passed the country through a Const intent to informationActivity

--- app/src/main/java/com/example/spyridonsaridakiscvapp/CountryInformationActivity.java
+++ app/src/main/java/com/example/spyridonsaridakiscvapp/CountryInformationActivity.java
@@ -2,7 +2,10 @@ package com.example.spyridonsaridakiscvapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class CountryInformationActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class CountryInformationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country_information);
+        TextView tvT = findViewById(R.id.tvCountryTitle);
+        Intent in = getIntent();
+        String code = in.getStringExtra(MapsHostActivity.CODEFORINFO);
+        if(code.equals("1")){
+            tvT.setText("Greece");
+        }else if(code.equals("2")){
+            tvT.setText("Cyprus");
+        }
+    }
+
+    public void backToMap(View v){
+        finish();
     }
 }
