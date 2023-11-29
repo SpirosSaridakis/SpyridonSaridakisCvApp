6b407c9de9d765a978ffbc45c7c5a72c1c08a25e
SpirosSaridakis <spiros.sar1234@hotmail.com>
2023-11-29 10:35:47 +0200

    Added phone speech and removed the settings option from the navigation drawer

--- app/src/main/java/com/example/spyridonsaridakiscvapp/MainActivity.java
+++ app/src/main/java/com/example/spyridonsaridakiscvapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.spyridonsaridakiscvapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.view.Menu;
 
@@ -17,10 +18,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.spyridonsaridakiscvapp.databinding.ActivityMainBinding;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    TextToSpeech t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +34,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR){
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Navigate the app using the navigation lines on the top left of the screen", Snackbar.LENGTH_LONG)
                         .setAnchorView(R.id.fab)
                         .setAction("Action", null).show();
+                speakNow();
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
@@ -53,16 +66,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void speakNow(){
+        t1.speak("Navigate the app using the navigation lines on the top left of the screen",TextToSpeech.QUEUE_FLUSH,null);
     }
 }
