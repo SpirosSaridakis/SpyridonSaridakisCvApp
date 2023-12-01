c9d03771a218fe40cc0ef6ab932bfd9fc0027f17
SpirosSaridakis <spiros.sar1234@hotmail.com>
2023-12-01 16:45:18 +0200

    Finished the volunteering fragment, also made a small change in the academic fragment because the margins were wrong

--- app/src/main/java/com/example/spyridonsaridakiscvapp/VolunteeringFragment.java
+++ app/src/main/java/com/example/spyridonsaridakiscvapp/VolunteeringFragment.java
@@ -22,7 +22,19 @@ public class VolunteeringFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        final TextView textView = binding.textSlideshow;
+        TextView tvTitle = root.findViewById(R.id.tvVolTitle);
+        tvTitle.setText("Volunteering is a big part of my life and I take great joy in helping the people around me, some of" +
+                " the most important places that i have volunteered in are the following:");
+        TextView tvContent = root.findViewById(R.id.tvContent);
+        tvContent.setText("•I was a volunteer at that 26th Panhellenic conference on informatics that has hosted by my university." +
+                "There i assisted the organizers in setting up the equipment for the live broadcast of the conference as well as " +
+                "showing around guests in the university campus. I also was a moderator for the online streaming of the event.\n\n"+
+                "•I volunteered at the TEDx University of Piraeus event where again I was responsible for setting up the equipment of a" +
+                " booth that broadcasted the event in the Metaverse platform of Meta. I also helped guests of the event navigate the virtual " +
+                "space.\n\n" +
+                "•After being a camp member of the Camp of the Holy Metropolis of Nikaia for 7 years, I was chosen to volunteer as a team leader in it." +
+                " I was responsible for looking after a team of middle school children for 15 days. I also participated in the cleaning and preparation" +
+                "of the campsite before and after the start of the camping season.");
         return root;
     }
 
